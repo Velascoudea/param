2ad44f8702cd964e0601bec4c5876734c413d1ae
Agustin <velaajustin28@gmail.com>
2025-09-28 19:22:18 -0600

    cambios

--- Operaciones.java
+++ Operaciones.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Operaciones {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingresa el primer número: ");
-        int num1 = sc.nextInt();
+    //    System.out.print("Ingresa el primer número: ");
+      //  int num1 = sc.nextInt();
 
-        System.out.print("Ingresa el segundo número: ");
-        int num2 = sc.nextInt();
+      //  System.out.print("Ingresa el segundo número: ");
+      //  int num2 = sc.nextInt();
 
-        int suma = num1 + num2;
+       // int suma = num1 + num2;
 
-        System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma);
+      //  System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma);
         
-        sc.close();
+       // sc.close();
+
+       int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Suma: " + (a+b));
     }
 }
