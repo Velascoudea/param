6a6bd28e4f2f40d5b2da288b0ea554b1943e7bd2
Agustin <velaajustin28@gmail.com>
2025-09-28 18:35:09 -0600

    comando

---
+++ Holamundo.java
@@ -0,0 +1,6 @@
+public class Holamundo{
+    public static void main(String[] args){
+        System.out.print("Hola Mundo 1");
+
+    }
+}
---
+++ Operaciones.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Operaciones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = sc.nextInt();
+
+        int suma = num1 + num2;
+
+        System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma);
+        
+        sc.close();
+    }
+}
